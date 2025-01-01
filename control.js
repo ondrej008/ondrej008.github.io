f05c0b33663ed299af3b9af43b138da7e6a74ba1
ondrej008 <ondra008@centrum.cz>
2025-01-01 02:22:19 +0100

    I hate this

--- control.js
+++ control.js
@@ -1,4 +1,35 @@
-window.addEventListener('load', function() {
-	var thing = document.getElementById('output');
-	thing.innerText = items + recipes;
+window.addEventListener("load", function() {
+	const itemCount = document.getElementById("itemCount");
+	const recipeCount = document.getElementById("recipeCount");
+	
+	itemCount.innerText = Object.keys(items).length + " items.";
+	recipeCount.innerText = Object.keys(recipes).length + " recipes.";
+	
+	
+	
+	const canvas = document.getElementById("render");
+	const ctx = canvas.getContext("2d");
+	
+	// Node is Amount, Recipe, x, y, w, h, Connections
+	const nodes = [];
+	
+	function resizeCanvas() {
+		canvas.width = window.innerWidth - 250;
+		canvas.height = window.innerHeight;
+		drawCanvas();
+	}
+	
+	function drawCanvas() {
+		if(nodes.length == 0) {
+			ctx.font = "96px serif";
+			ctx.textAlign = "center";
+			ctx.fillText("Get started by adding\na Recipe!", canvas.width / 2, canvas.height / 2);
+			return;
+		}
+		
+		
+	}
+	
+	resizeCanvas();
+	window.addEventListener("resize", resizeCanvas);
 });
